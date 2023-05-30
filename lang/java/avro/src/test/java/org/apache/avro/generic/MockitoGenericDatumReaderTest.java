50341fa89bd8c4c2bdaddf8f991f0861c896ec3d
ironmarken <ferrimarco98@gmail.com>
2023-05-30 10:39:00 +0200

    Dummy Mockito test

---
+++ lang/java/avro/src/test/java/org/apache/avro/generic/MockitoGenericDatumReaderTest.java
@@ -0,0 +1,30 @@
+package org.apache.avro.generic;
+
+import org.apache.avro.Schema;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+//@RunWith(MockitoJUnitRunner.class)
+public class MockitoGenericDatumReaderTest {
+
+  Schema mockedSchema;
+  GenericDatumReader genericDatumReader;
+
+  @Before
+  public void setup() {
+    mockedSchema = Mockito.mock(Schema.class);
+    genericDatumReader = new GenericDatumReader(mockedSchema, mockedSchema);
+
+  }
+
+  @Test
+  public void dummyMockitoTest() {
+    Schema schema = genericDatumReader.getSchema();
+    Assert.assertEquals(mockedSchema, schema);
+  }
+
+}
